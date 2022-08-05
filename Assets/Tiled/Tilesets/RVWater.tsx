6fc7177a884284897564b5bfda5d8f8afc23c895
MaCabezon <mcabezonarias@gmail.com>
2022-08-05 17:48:26 +0200

    Forest map art changed

---
+++ Assets/Tiled/Tilesets/RVWater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="RVWater" tilewidth="16" tileheight="16" tilecount="286" columns="13">
+ <image source="../../3rd Party/Sprites/World of Solaria Rural Village Update 06/16x16/Tilesets/RVWater.png" width="208" height="352"/>
+</tileset>
