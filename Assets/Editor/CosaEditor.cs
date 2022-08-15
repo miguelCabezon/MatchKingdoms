7ca8b13103e6382a06767f1e87131eb332458c0a
MaCabezon <mcabezonarias@gmail.com>
2022-08-15 12:50:45 +0200

    Logo

---
+++ Assets/Editor/CosaEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.VersionControl;
+
+[CustomEditor(typeof(Tile))]
+public class CosaEditor : Editor
+{
+
+    //GUI - todo en cualquier sitio
+    // GUILayout
+    //GUILayout
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("hola", MessageType.Info);
+
+        GUILayout.BeginHorizontal();
+            GUILayout.Label("Property 01");
+            GUILayout.TextField("Escriba");
+        GUILayout.EndHorizontal();
+
+        // Tile td = (Tile)target;
+
+        // GUI.enabled = t.horizontalSpeed < 0f;
+
+    }
+}
